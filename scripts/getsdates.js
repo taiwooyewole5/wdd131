7dfe035bb99534164700818f7cc785bf11647ac3
taiwooyewole5 <taiwooyewole453@gmail.com>
2025-09-06 14:14:25 +0100

    Js added and corrected

--- scripts/getsdates.js
+++ scripts/getsdates.js
@@ -1,18 +1,17 @@
-<script>
-  // Get the last modified date of the document
-  const lastModifiedDate = new Date(document.lastModified);
+// Get the last modified date of the document
+const lastModifiedDate = new Date(document.lastModified);
 
-  // Format it nicely: DD/MM/YYYY HH:MM:SS
-  const options = { 
-    day: '06', 
-    month: '09', 
-    year: '2025', 
-    hour: '14', 
-    minute: '08', 
-    second: '32'
-  };
-  const formattedDate = lastModifiedDate.toLocaleDateString('en-GB', options).replace(',', '');
+// Format it nicely: DD/MM/YYYY HH:MM:SS
+const options = { 
+  day: '2-digit', 
+  month: '2-digit', 
+  year: 'numeric', 
+  hour: '2-digit', 
+  minute: '2-digit', 
+  second: '2-digit',
+  hour12: false
+};
+const formattedDate = lastModifiedDate.toLocaleDateString('en-GB', options).replace(',', '');
 
-  // Update the footer text
-  document.getElementById("lastModified").textContent = "Last Modification: " + formattedDate;
-</script>
+// Update the footer text
+document.getElementById("lastModified").textContent = "Last Modification: " + formattedDate;
